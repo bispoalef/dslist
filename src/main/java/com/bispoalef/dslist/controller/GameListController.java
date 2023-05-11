7bf320d5b3fc4b702acc41c18bab8b50f8d19ee2
bispoalef <alef.b.dos.santos.1993@gmail.com>
2023-05-10 23:27:13 -0300

    mudando nome

--- src/main/java/com/bispoalef/dslist/controller/GameListController.java
+++ src/main/java/com/bispoalef/dslist/controller/GameListController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/gameList")
+@RequestMapping(value = "/gamelist")
 public class GameListController {
 
     @Autowired
